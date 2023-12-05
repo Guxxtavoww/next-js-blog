14ff9f9a42e41cc8cb497c501e8c0a5b4bb1b324
Gustavo <gustavoaugustocar@hotmail.com>
2023-12-05 16:23:12 -0300

    Github login setup

--- src/components/nav/profile.tsx
+++ src/components/nav/profile.tsx
@@ -4,19 +4,35 @@ import React from 'react';
 import Image from 'next/image';
 import { User } from '@supabase/supabase-js';
 
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+
 interface iProfileProps {
   userData: User;
 }
 
 export default function Profile({ userData }: iProfileProps) {
   return (
-    <Image
-      src={userData.user_metadata?.avatar_url}
-      alt="Profile Pic"
-      width={50}
-      height={50}
-      className="rounded-full ring-2 ring-green-500"
-      loading="lazy"
-    />
+    <Popover>
+      <PopoverTrigger>
+        <Image
+          src={userData.user_metadata?.avatar_url}
+          alt="Profile Pic"
+          width={50}
+          height={50}
+          className="rounded-full ring-2 ring-green-500"
+          loading="lazy"
+        />
+      </PopoverTrigger>
+      <PopoverContent>
+        <div className="px-4 text-sm">
+          <p>{userData.user_metadata?.user_name}</p>
+          <p className="text-gray-500">{userData.email}</p>
+        </div>
+      </PopoverContent>
+    </Popover>
   );
 }
