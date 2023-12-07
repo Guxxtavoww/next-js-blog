2ff58505b854a0af29a09e6b0c3cf1e7cfbec924
Gustavo <gustavoaugustocar@hotmail.com>
2023-12-07 16:05:34 -0300

    Chore: Adding Supabase cli and database types

--- src/app/dashboard/post/create/schemas/create-post-form.schema.ts
+++ src/app/dashboard/post/create/schemas/create-post-form.schema.ts
@@ -1,33 +1,15 @@
 import { z } from 'zod';
 
-export const createPostFormSchema = z
-  .object({
-    title: z.string({ required_error: 'Título é obrigatório' }).min(10, {
-      message: 'Título é muito pequeno',
-    }),
-    content: z.string({ required_error: 'Conteúdo é obrigatório' }).min(50, {
-      message: 'Conteúdo é muito pequeno',
-    }),
-    image_url: z.string({ required_error: 'URL da imagem é obrigatória' }).url({
-      message: 'Url inválida',
-    }),
-    is_premium: z.boolean().optional().default(false),
-    is_published: z.boolean().optional().default(false),
-  })
-  .refine(
-    (data) => {
-      const image_url = data.image_url;
-
-      try {
-        const url = new URL(image_url);
-
-        return url.hostname === 'images.unsplash.com';
-      } catch {
-        return false;
-      }
-    },
-    {
-      message: 'Atualmete só aceitamos imagens do do site unsplash',
-      path: ['image_url'],
-    }
-  );
+export const createPostFormSchema = z.object({
+  title: z.string({ required_error: 'Título é obrigatório' }).min(10, {
+    message: 'Título é muito pequeno',
+  }),
+  content: z.string({ required_error: 'Conteúdo é obrigatório' }).min(50, {
+    message: 'Conteúdo é muito pequeno',
+  }),
+  image_url: z.string({ required_error: 'URL da imagem é obrigatória' }).url({
+    message: 'Url inválida',
+  }),
+  is_premium: z.boolean().optional().default(false),
+  is_published: z.boolean().optional().default(false),
+});
