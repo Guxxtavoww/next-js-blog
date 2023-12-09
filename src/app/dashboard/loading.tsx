e9a82e756703e28d7265652cfa3759085c7590ec
Gustavo <gustavoaugustocar@hotmail.com>
2023-12-09 16:39:19 -0300

    FEAT: Update post logic done

--- src/app/dashboard/loading.tsx
+++ src/app/dashboard/loading.tsx
@@ -2,14 +2,8 @@
 
 import React from 'react';
 
+import { Skeleton } from '@/components/ui/skeleton';
+
 export default function Loading() {
-  return (
-    <div className=" animate-pulse space-y-5">
-      <div className="flex justify-between items-center">
-        <h1 className=" h-10 w-56 bg-graident-dark rounded-md"></h1>
-        <h1 className=" h-10  w-48 bg-graident-dark rounded-md"></h1>
-      </div>
-      <div className=" border h-96 rounded-md bg-graident-dark "></div>
-    </div>
-  );
+  return <Skeleton className="w-full h-10 rounded-md" />;
 }
